6c148424d76cd2abb519f9cfeddc7b5d34666d0b
Da1man <chaos4ever@mail.ru>
2020-05-01 16:44:02 +0300

    lesson 115 done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
+import {Search} from "../components/Search";
+import {Card} from "../components/Card";
 
 export const Home = () => {
+  const cards = new Array(15)
+    .fill('')
+    .map((_, i) => i)
+  console.log(cards)
   return(
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <React.Fragment>
+      <Search />
+
+      <div className={'row'}>
+
+        {cards.map( (card, index) => {
+          return (
+            <div className="col-sm-4 mb-4" key={index}>
+              <Card/>
+            </div>
+          )
+        })}
+
+      </div>
+
+    </React.Fragment>
   )
 };
